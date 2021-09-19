4727f7f2eaa57282978ea2a64442cd63f0c79d65
Miryxx <alexisreedy@gmail.com>
2021-09-19 13:51:34 -0400

    September 19, 2021 1:51 PM

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -58,7 +58,7 @@ public class PlayerController : MonoBehaviour
 
             SetCountText();
         }
-        else if(other.gameObject.CompareTag("Enemy"))
+        else if (other.gameObject.CompareTag("Enemy"))
         {
             other.gameObject.SetActive(false);
             count = count - 1;
